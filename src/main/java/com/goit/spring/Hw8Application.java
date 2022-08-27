141c78191c613ff8a2794e3e21b2fc69bceb8cca
DadaYaArtem <62649858+DadaYaArtem@users.noreply.github.com>
2022-08-27 12:30:21 +0300

    Update Hw8Application.java

--- src/main/java/com/goit/spring/Hw8Application.java
+++ src/main/java/com/goit/spring/Hw8Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class Hw8Application {
 
 	public static void main(String[] args) {
